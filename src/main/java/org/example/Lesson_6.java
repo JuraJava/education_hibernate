c889f523361ff47e97577a5795bc0342dc9ddd22
Jura Danilov <jura.danilov.2024@gmail.com>
2025-01-07 19:31:17 +0300

    Initial commit

---
+++ src/main/java/org/example/Lesson_6.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.example.entity.Car;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Lesson_6 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+//                .addAnnotatedClass(Car.class)
+//  Эту строку можно не писать, т.к. информацию об этом
+//  мы указали в hibernate.cfg.xml
+                .buildSessionFactory();
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            Car car1 = session.get(Car.class, 4);
+            System.out.println(car1);
+            // после того как мы получили данные об объекте из БД мы с помощью
+            // сеттера можем их переустановить в другое значение
+            car1.setSpeed(240.0);
+            car1.setName("Mercedes");
+            car1.setPrice(200_000);
+            car1.setColor("Black");
+            System.out.println(car1);
+
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
+    }
+}
